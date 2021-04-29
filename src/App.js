ebb18524c2df60c6163f900372c0616c69c4403c
Raza Ahmed <rzahmed@ucdavis.edu>
2021-04-28 21:10:31 -0700

    form and routing frontend

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imagelinkform/ImageLinkForm";
 import Rank from "./components/rank/Rank";
 import Clarifai from "clarifai";
+import SignIn from "./components/signin/SignIn";
+import Register from "./components/register/Register";
 
 const app = new Clarifai.App({
 	apiKey: "c224c3964124468c83ab16bfdbd1234e",
@@ -14,6 +16,9 @@ const app = new Clarifai.App({
 function App() {
 	const [input, setInput] = useState("");
 	const [imageUrl, setImageUrl] = useState("");
+	const [box, setBox] = useState({});
+	const [route, setRoute] = useState("signin");
+	const [signedIn, setSignedIn] = useState(false);
 
 	const onInputChange = (e) => {
 		setInput(e.target.value);
@@ -21,25 +26,58 @@ function App() {
 
 	const onSubmit = () => {
 		setImageUrl(input);
-		app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
-			function (response) {
-				console.log(
-					response.outputs[0].data.regions[0].region_info.bounding_box
-				);
-			},
-			function (err) {
-				// body
-			}
-		);
+		app.models
+			.predict(Clarifai.FACE_DETECT_MODEL, input)
+			.then((response) => displayFaceBox(calcFaceLocation(response)))
+			.catch((error) => console.log(error));
+	};
+
+	const calcFaceLocation = (data) => {
+		const clarifaiFace =
+			data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById("inputImg");
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - clarifaiFace.right_col * width,
+			bottomRow: height - clarifaiFace.bottom_row * height,
+		};
+	};
+
+	const displayFaceBox = (box) => {
+		setBox(box);
+	};
+
+	const onRouteChange = (route) => {
+		if (route === "signout") {
+			setSignedIn(false);
+		} else if (route === "home") {
+			setSignedIn(true);
+		}
+		setRoute(route);
 	};
 
 	return (
 		<div className="App">
-			<Navigation />
-			<Logo />
-			<Rank />
-			<ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onSubmit} />
-			<FaceRecognition imageUrl={imageUrl} />
+			<Navigation onRouteChange={onRouteChange} isSignedIn={signedIn} />
+			{route === "home" ? (
+				<div>
+					{" "}
+					<Logo />
+					<Rank />
+					<ImageLinkForm
+						onInputChange={onInputChange}
+						onButtonSubmit={onSubmit}
+					/>
+					<FaceRecognition box={box} imageUrl={imageUrl} />
+				</div>
+			) : route === "signin" ? (
+				<SignIn onRouteChange={onRouteChange} />
+			) : (
+				<Register onRouteChange={onRouteChange} />
+			)}
 		</div>
 	);
 }
--- src/components/facerecognition/FaceRecognition.js
+++ src/components/facerecognition/FaceRecognition.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./facerecognition.css";
 
-function FaceRecognition({ imageUrl }) {
+function FaceRecognition({ box, imageUrl }) {
 	return (
 		<div className="center ma">
 			<div className="absolute mt2">
-				<img src={imageUrl} alt="" width="500px" height="auto" />
+				<img src={imageUrl} alt="" width="500px" height="auto" id="inputImg" />
+				<div
+					className="bounding_box"
+					style={{
+						top: box.topRow,
+						right: box.rightCol,
+						bottom: box.bottomRow,
+						left: box.leftCol,
+					}}></div>
 			</div>
 		</div>
 	);
